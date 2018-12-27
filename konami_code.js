fcb2db69598401a45a4d6f5b1ffb6a10297bdc7e
Holden Altaffer <haltaffer@gmail.com>
2018-12-27 00:08:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ input.addEventListener('keydown', function(e) {
 
 
     if (key === konami[index]) {
-      index = index + 1;
+      index++;
 
       if (index === konami.length) {
         alert("Hurray!");
