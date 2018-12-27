9c5c99266a77204129921e40707482866cf221ad
Holden Altaffer <haltaffer@gmail.com>
2018-12-27 00:10:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const input = document.body
+  let index = 0;
+input.addEventListener('keydown', function(e) {
+  
+
+  let key = parseInt(e.detail);
+
+
+    if (key === konami[index]) {
+      index++;
+
+      if (index === konami.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    console.log(index, e.detail)
+    });
 
 }
